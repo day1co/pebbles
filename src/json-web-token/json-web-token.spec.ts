4f6da13e27beeccc68c6fe434877b4bb50094b60
alstjs1207 <alstjs1207@naver.com>
2021-10-07 17:02:00 +0900

    util: add decode test code to return null for invalid

--- src/json-web-token/json-web-token.spec.ts
+++ src/json-web-token/json-web-token.spec.ts
@@ -14,45 +14,55 @@ describe('JsonWebToken', () => {
     expect(typeof token).toBe('string');
     expect(len).toBe(3);
   });
-  it('verify token', () => {
-    const key = 'key';
-    const payload = {
-      t1: 't1',
-      t2: 't2',
-    };
-    const token = jwt.createJwt(key, payload);
-    const result = jwt.verifyJwt(token, key);
-
-    expect(result).toMatchObject(payload);
-  });
-  it('verify invalid token', () => {
-    try {
+  describe('verify token', () => {
+    it('should return payload for token', () => {
       const key = 'key';
-      const wrong_key = 'wrong_key';
       const payload = {
         t1: 't1',
         t2: 't2',
       };
       const token = jwt.createJwt(key, payload);
-      jwt.verifyJwt(token, wrong_key);
-    } catch (exception) {
-      //ts v4.4 부터 exception object가 unknown type으로 정의
-      if (exception instanceof Error) {
-        expect(exception.message).toBe('invalid signature');
-      }
-    }
-  });
-  it('decoded token', () => {
-    const key = 'key';
-    const header = { alg: 'HS256', typ: 'JWT' };
-    const payload = {
-      t1: 't1',
-      t2: 't2',
-    };
-    const token = jwt.createJwt(key, payload);
-    const result = jwt.decodeJwt(token);
+      const result = jwt.verifyJwt(token, key);
 
-    expect(result!.header).toMatchObject(header);
-    expect(result!.payload).toMatchObject(payload);
+      expect(result).toMatchObject(payload);
+    });
+    it('should return invalid for wrong key', () => {
+      try {
+        const key = 'key';
+        const wrong_key = 'wrong_key';
+        const payload = {
+          t1: 't1',
+          t2: 't2',
+        };
+        const token = jwt.createJwt(key, payload);
+        jwt.verifyJwt(token, wrong_key);
+      } catch (exception) {
+        //ts v4.4 부터 exception object가 unknown type으로 정의
+        if (exception instanceof Error) {
+          expect(exception.message).toBe('invalid signature');
+        }
+      }
+    });
+  });
+  describe('decode token', () => {
+    it('should return header and payload for decode', () => {
+      const key = 'key';
+      const header = { alg: 'HS256', typ: 'JWT' };
+      const payload = {
+        t1: 't1',
+        t2: 't2',
+      };
+      const token = jwt.createJwt(key, payload);
+      const result = jwt.decodeJwt(token);
+
+      expect(result!.header).toMatchObject(header);
+      expect(result!.payload).toMatchObject(payload);
+    });
+    it('should return null for invalid', () => {
+      const token = '__invalid__';
+      const result = jwt.decodeJwt(token);
+
+      expect(result).toBeNull();
+    });
   });
 });
