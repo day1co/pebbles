e5dd84b6968ea100847f4a6dbaef3cb2895f0384
alstjs1207 <alstjs1207@naver.com>
2021-10-10 20:12:12 +0900

    util: add interface json-web-token and type
    option, header, payload, jwt 인터페이스 추가 및 algorithm type 추가

--- src/json-web-token/json-web-token.ts
+++ src/json-web-token/json-web-token.ts
@@ -1,19 +1,20 @@
 import * as jwt from 'jsonwebtoken';
+import type { IJwt, IOptions, IPayload } from './json-web-token.interface';
 
 export namespace JsonWebToken {
   export function createJwt(
     key: string,
     payload: string | Record<string, unknown>,
-    options?: jwt.SignOptions | undefined
+    options?: IOptions | undefined
   ): string {
     return jwt.sign(payload, key, options);
   }
 
-  export function verifyJwt(token: string, key: string): string | jwt.JwtPayload {
+  export function verifyJwt(token: string, key: string): string | IPayload {
     return jwt.verify(token, key);
   }
 
-  export function decodeJwt(token: string): jwt.Jwt | null {
+  export function decodeJwt(token: string): IJwt | null {
     return jwt.decode(token, { complete: true });
   }
 }
---
+++ src/json-web-token/json-web-token.type.ts
@@ -0,0 +1,14 @@
+export type Algorithm =
+  | 'HS256'
+  | 'HS384'
+  | 'HS512'
+  | 'RS256'
+  | 'RS384'
+  | 'RS512'
+  | 'ES256'
+  | 'ES384'
+  | 'ES512'
+  | 'PS256'
+  | 'PS384'
+  | 'PS512'
+  | 'none';
