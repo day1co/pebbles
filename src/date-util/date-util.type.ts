0fa50cf4923b406db3d85703c5c32244a6360f20
변경연 <kissob4905@gmail.com>
2021-10-11 11:34:59 +0900

    date-util: type에서 interface 분리

--- src/date-util/date-util.type.ts
+++ src/date-util/date-util.type.ts
@@ -1,8 +1,1 @@
-export type CalcDatetimeOpts = {
-  year?: number;
-  month?: number;
-  date?: number;
-  hour?: number;
-  minute?: number;
-  second?: number;
-};
+export type DateType = string | number | Date;
