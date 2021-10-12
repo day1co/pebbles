b3f494a46071c93b432ad18bd4821fc812334ba4
alstjs1207 <alstjs1207@naver.com>
2021-10-12 15:28:38 +0900

    util: remove Ipayload

--- src/json-web-token/json-web-token.ts
+++ src/json-web-token/json-web-token.ts
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken';
-import type { IJwt, IOptions, IPayload } from './json-web-token.interface';
+import type { IJwt, IOptions } from './json-web-token.interface';
 
 export namespace JsonWebToken {
   export function createJwt(key: string, payload: string | Record<string, unknown>, options?: IOptions): string {
     return jwt.sign(payload, key, options);
   }
 
-  export function verifyJwt(token: string, key: string): string | IPayload {
+  export function verifyJwt(token: string, key: string): string | Record<string, unknown> {
     return jwt.verify(token, key);
   }
 
