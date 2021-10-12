5b4dc872e30ca06e4d8f4571873a2d66f6e434bf
alstjs1207 <alstjs1207@naver.com>
2021-10-12 09:54:30 +0900

    util: remove unnecessary undefined and interface
    불필요한 인터페이스의 함수 파라미터 삭제함.
    불필요한 optional undefined 삭제함.

--- src/json-web-token/json-web-token.interface.ts
+++ src/json-web-token/json-web-token.interface.ts
@@ -1,42 +1,17 @@
 import { Algorithm } from './json-web-token.type';
 
 export interface IOptions {
-  algorithm?: Algorithm | undefined;
-  keyid?: string | undefined;
-  expiresIn?: string | number | undefined;
-  notBefore?: string | number | undefined;
-  audience?: string | string[] | undefined;
-  subject?: string | undefined;
-  issuer?: string | undefined;
-  jwtid?: string | undefined;
-  mutatePayload?: boolean | undefined;
-  noTimestamp?: boolean | undefined;
-  header?: IHeader | undefined;
-  encoding?: string | undefined;
+  algorithm?: Algorithm;
+  header?: IHeader;
 }
 
 export interface IHeader {
   alg: string | Algorithm;
-  typ?: string | undefined;
-  cty?: string | undefined;
-  crit?: Array<string | Exclude<keyof IHeader, 'crit'>> | undefined;
-  kid?: string | undefined;
-  jku?: string | undefined;
-  x5u?: string | string[] | undefined;
-  'x5t#S256'?: string | undefined;
-  x5t?: string | undefined;
-  x5c?: string | string[] | undefined;
+  typ?: string;
 }
 
 export interface IPayload {
   [key: string]: unknown;
-  iss?: string | undefined;
-  sub?: string | undefined;
-  aud?: string | string[] | undefined;
-  exp?: number | undefined;
-  nbf?: number | undefined;
-  iat?: number | undefined;
-  jti?: string | undefined;
 }
 
 export interface IJwt {
