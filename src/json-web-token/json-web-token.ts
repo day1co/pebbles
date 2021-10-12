dbd4abd77a7583ce3ce6c25d25b8891c19fe604d
alstjs1207 <alstjs1207@naver.com>
2021-10-12 09:54:30 +0900

    util: remove unnecessary undefined and interface
    불필요한 인터페이스의 함수 파라미터 삭제함.
    불필요한 optional undefined 삭제함.

--- src/json-web-token/json-web-token.ts
+++ src/json-web-token/json-web-token.ts
@@ -2,11 +2,7 @@ import * as jwt from 'jsonwebtoken';
 import type { IJwt, IOptions, IPayload } from './json-web-token.interface';
 
 export namespace JsonWebToken {
-  export function createJwt(
-    key: string,
-    payload: string | Record<string, unknown>,
-    options?: IOptions | undefined
-  ): string {
+  export function createJwt(key: string, payload: string | Record<string, unknown>, options?: IOptions): string {
     return jwt.sign(payload, key, options);
   }
 
