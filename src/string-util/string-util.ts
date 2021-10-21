88c75fa528fd9ad65ab153d10beb61a8821c5d1c
moonimooni <munhw1408@gmail.com>
2021-10-21 15:13:07 +0900

    fix: substringByByte 함수 이름 변경 및 escape  사용 삭제

--- src/string-util/string-util.ts
+++ src/string-util/string-util.ts
@@ -1,49 +1,33 @@
+function getCharacterByteInEUCKR(character: string): number {
+  return character.charCodeAt(0) > 127 ? 2 : 1;
+}
+
+function getStringByteInEUCKR(string: string): number {
+  let byte = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    byte += getCharacterByteInEUCKR(string[i]);
+  }
+  return byte;
+}
+
+function substringByByteInEUCKR(string: string, byteLength: number): string {
+  let i = 0;
+
+  for (let byte = 0; byte <= byteLength; i++) {
+    byte += getCharacterByteInEUCKR(string[i]);
+  }
+  return string.substring(0, i - 1);
+}
+
 export namespace StringUtil {
   export function compactTextMessage(fullText: string, textToBeTrimmed: string, maxByteLength = 90): string {
-    if (getByteLength(fullText) <= maxByteLength) {
+    if (getStringByteInEUCKR(fullText) <= maxByteLength) {
       return fullText;
     }
 
-    const minByteLength = getByteLength(fullText.replace(textToBeTrimmed, '...'));
-    const trimmedText = substringByByte(textToBeTrimmed, maxByteLength - minByteLength);
-
+    const minByteLength = getStringByteInEUCKR(fullText.replace(textToBeTrimmed, '...'));
+    const trimmedText = substringByByteInEUCKR(textToBeTrimmed, maxByteLength - minByteLength);
     return fullText.replace(textToBeTrimmed, trimmedText + '...');
-  }
-
-  /**@private */
-  function getByteLength(string: string): number {
-    let byte = 0;
-    let decimalUnicode: number;
-
-    for (let i = 0; (decimalUnicode = string.charCodeAt(i)); i++) {
-      byte += decimalUnicode >> 11 ? 2 : 1;
-    }
-    return byte;
-  }
-
-  /**@private */
-  function substringByByte(string: string, byteLength: number): string {
-    let byte = 0;
-    let i = 0;
-
-    for (const lengthOfString = string.length; i < lengthOfString; i++) {
-      const encodedChar = escape(string.charAt(i));
-
-      // 인코딩 결과값의 길이가 4가 넘으면 ASCII 문자에 해당하지 않음.
-      if (encodedChar.length >= 4) {
-        byte += 2;
-      } else if (encodedChar !== '%0D') {
-        byte++;
-      }
-
-      if (byte > byteLength) {
-        // \n일 경우
-        if (encodedChar === '%0A') {
-          i--;
-        }
-        break;
-      }
-    }
-    return string.substring(0, i);
   }
 }
