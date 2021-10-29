9acc4e45f992b99d83bd9cd5a5e7f89b141c8f0c
Hyun Jin Kim <costa.kim@day1company.co.kr>
2021-10-29 17:23:26 +0900

    DateUtil#parseTimestamp, `fmt` 파라미터에서 제거

--- src/date-util/date-util.ts
+++ src/date-util/date-util.ts
@@ -143,7 +143,9 @@ export namespace DateUtil {
     return min;
   }
 
-  export function parseTimestamp(str: string, fmt = 'YYYYMMDDHHmmssSSS'): Date {
+  export function parseTimestamp(str: string): Date {
+    const fmt = 'YYYYMMDDHHmmssSSS';
+
     if (str.length > fmt.length) {
       throw new Error(`Invalid Arguments: str and fmt are not matched. str: ${str}, fmt: ${fmt}`);
     }
@@ -205,9 +207,9 @@ export namespace DateUtil {
         },
       ],
       [
-        'S{1,9}',
+        'S?S?S',
         (match, date) => {
-          date.setMilliseconds(parseInt(substrByMatch(match)) / 1000);
+          date.setMilliseconds(parseInt(substrByMatch(match)));
           return date;
         },
       ],
