d14b68afc98803881646e21de12fbaae6e3262ad
moonimooni <munhw1408@gmail.com>
2021-11-03 15:20:49 +0900

    fix: numberUtil.valueOf의 중복된 isNaN 사용 제거

--- src/number-util/number-util.ts
+++ src/number-util/number-util.ts
@@ -1,23 +1,19 @@
 export namespace NumberUtil {
   export function intValueOf(numStr: string): number {
-    const result = isNumeric(numStr) ? Number(numStr) : NaN;
+    const result = Number(numStr);
 
-    if (Number.isNaN(result) || !Number.isInteger(result)) {
+    if (!numStr || !Number.isInteger(result)) {
       throw new Error(`unable to convert "${numStr}" to integer type`);
     }
     return result;
   }
 
   export function valueOf(numStr: string): number {
-    const result = isNumeric(numStr) ? Number(numStr) : NaN;
+    const result = Number(numStr);
 
-    if (Number.isNaN(result) || !Number.isFinite(result)) {
-      throw new Error(`unable to convert "${numStr}" to integer type`);
+    if (!numStr || !Number.isFinite(result)) {
+      throw new Error(`unable to convert "${numStr}" to number type`);
     }
     return result;
   }
-}
-
-function isNumeric(str: string): boolean {
-  return !Number.isNaN(Number(str)) && !Number.isNaN(parseInt(str));
 }
