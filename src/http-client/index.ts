b41e82e65472e7fc523dc133143918a7081769f2
moonimooni <munhw1408@gmail.com>
2021-11-10 14:38:30 +0900

    http-client와 http-state를 별도로 분리

---
+++ src/http-client/index.ts
@@ -0,0 +1,2 @@
+export { HttpClient } from './http-client';
+export type { HttpReqConfig, HttpRes } from './http-client.interface';
--- src/http-util/index.ts
+++
@@ -1,3 +0,0 @@
-export { HttpClient } from './http-client/http-client';
-export { HttpState } from './http-state/http-state';
-export type { HttpReqConfig, HttpRes } from './http-client/http-client.interface';
