099154ad220c08e1bfc6376a7cda9f3e49df7fb8
moonimooni <munhw1408@gmail.com>
2021-11-10 12:56:16 +0900

    feat: date-util ISOFormat enum 추가

---
+++ src/date-util/date-util.enum.ts
@@ -0,0 +1,12 @@
+export enum ISOFormat {
+  'YYYY' = 'YYYY',
+  'YYYYMMDD' = 'YYYYMMDD',
+  'YYYY-MM' = 'YYYY-MM',
+  'YYYY-MM-DD' = 'YYYY-MM-DD',
+  'YYYY-MM-DDTHH:mm:ss' = 'YYYY-MM-DDTHH:mm:ss',
+  'YYYY-MM-DDTHH:mm:ss.SSS' = 'YYYY-MM-DDTHH:mm:ss.SSS',
+  'HH:mm:ss.SSS' = 'HH:mm:ss.SSS',
+  'HH:mm:ss' = 'HH:mm:ss',
+  'HH:mm' = 'HH:mm',
+  'HH' = 'HH',
+}
