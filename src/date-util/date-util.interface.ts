6e71867b59cb01b06419746901d9b4ec93a1bb5d
Ilu Han <iuhan@day1company.co.kr>
2021-12-02 10:56:03 +0900

    fix: 입력인자 interface 멤버들을 readonly로 변경

--- src/date-util/date-util.interface.ts
+++ src/date-util/date-util.interface.ts
@@ -1,8 +1,8 @@
 export interface CalcDatetimeOpts {
-  year?: number;
-  month?: number;
-  date?: number;
-  hour?: number;
-  minute?: number;
-  second?: number;
+  readonly year?: number;
+  readonly month?: number;
+  readonly date?: number;
+  readonly hour?: number;
+  readonly minute?: number;
+  readonly second?: number;
 }
