9ce07e1d2db9dd4eafb9ce603dd17fc8c8558f87
Ilu Han <iuhan@day1company.co.kr>
2021-12-08 00:14:51 +0900

    feat: tsc outDir이 프로젝트 root가 되도록 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
     node: true,
   },
   extends: ['plugin:prettier/recommended', 'prettier', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ['dist'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
