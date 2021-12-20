91966ef1b1af7138acf0d96d3dbef63f972a30c7
변경연 <kissob4905@gmail.com>
2021-12-20 09:26:49 +0900

    workflow: passing hook url when calling send slack func

--- .github/npm-publish-slack.sh
+++ .github/npm-publish-slack.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 slackToDeptDev() {
+  curl \
+    -X POST \
+    --data-urlencode \
+    "payload={\"channel\": \"#npm-publish-notify\", \"text\": \"$1\", \"attachments\":[{ \"fields\":[{\"title\":\"Notes\",\"value\":\"$2\"  }] }] }" $3
 }
 
 PACKAGE_NAME=$(jq -r .name package.json)
 PACKAGE_VERSION=$(jq -r .version package.json)
 echo "${PACKAGE_NAME}@${PACKAGE_VERSION} PUBLISH SUCCESS"
 
-slackToDeptDev "\`${PACKAGE_NAME}@${PACKAGE_VERSION}\` 이 배포되었습니다. <!here>" "담당자는 확인해주세요!"
+slackToDeptDev "\`${PACKAGE_NAME}@${PACKAGE_VERSION}\` 이 배포되었습니다. <!here>" "담당자는 확인해주세요!" $1
