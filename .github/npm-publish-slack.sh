d70af5d58f5f85a31f20206334417efe673acd9c
문희원 <munhuiwon@munhuiwons-MacBook-Pro.local>
2022-01-05 15:48:18 +0900

    workflow: npm-publish-slack-message에 change log가 포함되도록 합니다

--- .github/npm-publish-slack.sh
+++ .github/npm-publish-slack.sh
@@ -11,4 +11,4 @@ PACKAGE_NAME=$(jq -r .name package.json)
 PACKAGE_VERSION=$(jq -r .version package.json)
 echo "${PACKAGE_NAME}@${PACKAGE_VERSION} PUBLISH SUCCESS"
 
-slackToDeptDev "\`${PACKAGE_NAME}@${PACKAGE_VERSION}\` 이 배포되었습니다. <!here>" "담당자는 확인해주세요!" $1
+slackToDeptDev "\`${PACKAGE_NAME}@${PACKAGE_VERSION}\` 이 배포되었습니다. <!here>" $1 $2
