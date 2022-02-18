493bf03fcec8c5d7210256506eee10f603046e09
Ilu Han <iuhan@day1company.co.kr>
2022-02-18 13:22:41 +0900

    feat: jest 설정과 tsconfig 변경

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  coverageDirectory: '../coverage',
   preset: 'ts-jest',
+  rootDir: 'src',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
 };
