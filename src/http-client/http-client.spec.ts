753b732ca0fd11d11bf6ded8883696cad077c17d
Ilu Han <iuhan@day1company.co.kr>
2022-03-24 13:54:34 +0900

    feat: http-client 테스트를 서버 없이도 동작하도록 수정

--- src/http-client/http-client.spec.ts
+++ src/http-client/http-client.spec.ts
@@ -1,55 +1,42 @@
-import http from 'http';
+import axios from 'axios';
 import { HttpClient } from './http-client';
 
 describe('HttpClient', () => {
   const TEST_SERVER = 'http://localhost:9999';
-  const TEST_PORT = 9999;
-  const server = http.createServer((req, res) => {
-    res.end(JSON.stringify('HttpClient test is OK'));
-  });
 
   test('Base URL get/set', () => {
     const httpClient = new HttpClient();
     httpClient.baseUrl = TEST_SERVER;
-    expect(httpClient.baseUrl === TEST_SERVER).toBe(true);
+    expect(httpClient.baseUrl).toBe(TEST_SERVER);
   });
 
   describe('Http communication', () => {
-    beforeAll((done) => {
-      server.listen(TEST_PORT, done);
-      server.on('clientError', (err, socket) => {
-        socket.end();
-      });
-    });
-
-    afterAll((done) => {
-      server.close(done);
-    });
+    beforeAll(() => jest.mock('axios'));
 
     const httpClient = new HttpClient(TEST_SERVER);
 
     it('Http GET test', async () => {
+      axios.get = jest.fn().mockResolvedValue('get');
       const result = await httpClient.sendGetRequest('/');
-      expect(result.status).toBe(200);
-      expect(result.data).toBe('HttpClient test is OK');
+      expect(result).toBe('get');
     });
 
     it('Http POST test', async () => {
+      axios.post = jest.fn().mockResolvedValue('post');
       const result = await httpClient.sendPostRequest('/', {});
-      expect(result.status).toBe(200);
-      expect(result.data).toBe('HttpClient test is OK');
+      expect(result).toBe('post');
     });
 
     it('Http PUT test', async () => {
+      axios.put = jest.fn().mockResolvedValue('put');
       const result = await httpClient.sendPutRequest('/', {});
-      expect(result.status).toBe(200);
-      expect(result.data).toBe('HttpClient test is OK');
+      expect(result).toBe('put');
     });
 
     it('Http DELETE test', async () => {
+      axios.delete = jest.fn().mockResolvedValue('delete');
       const result = await httpClient.sendDeleteRequest('/', {});
-      expect(result.status).toBe(200);
-      expect(result.data).toBe('HttpClient test is OK');
+      expect(result).toBe('delete');
     });
   });
 });
