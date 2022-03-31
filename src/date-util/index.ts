a3df21c073e52fc3edf86cdc28f494fbe59fff79
Ilu Han <iuhan@day1company.co.kr>
2022-03-31 18:13:07 +0900

    feat: import 오류 수정

--- src/date-util/index.ts
+++ src/date-util/index.ts
@@ -1,8 +1,3 @@
 export { DateUtil } from './date-util';
-export type { DateType } from './date-util.type';
-export type {
-  CalcDatetimeOpts,
-  DateFormatOpts,
-  ISODateFormatOpts,
-  LocalDateTimeFormatOpts,
-} from './date-util.interface';
+export type { CalcDatetimeOpts, DateType } from './date-util.type';
+export type { DateFormatOpts, ISODateFormatOpts, LocalDateTimeFormatOpts } from './date-util.interface';
