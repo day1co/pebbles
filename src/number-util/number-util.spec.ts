b78860b183863d65eb85662bd66bcb362d68be4c
Dongsu Jang <77693+iolo@users.noreply.github.com>
2022-11-08 22:11:22 +0900

    fix isNumberic runtime error on edge case

--- src/number-util/number-util.spec.ts
+++ src/number-util/number-util.spec.ts
@@ -46,11 +46,11 @@ describe('NumberUtil', () => {
       expect(NumberUtil.isNumeric('0.1')).toEqual(true);
       expect(NumberUtil.isNumeric('-10')).toEqual(true);
       expect(NumberUtil.isNumeric('-10.1231234')).toEqual(true);
-      expect(NumberUtil.isNumeric('0xa')).toEqual(true);
     });
     it('should not equal to all', () => {
       expect(NumberUtil.isNumeric('a')).toEqual(false);
       expect(NumberUtil.isNumeric('0a')).toEqual(false);
+      expect(NumberUtil.isNumeric('0xa')).toEqual(false);
       expect(NumberUtil.isNumeric('sdfsdfsdfa')).toEqual(false);
       expect(NumberUtil.isNumeric('\\http')).toEqual(false);
       expect(NumberUtil.isNumeric('1.2.3')).toEqual(false);
