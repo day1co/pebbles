073654534a81f3b88f42021153da58686cd2dfcf
seunghwan <tmdghks0217@gmail.com>
2023-02-08 16:23:38 +0900

    reflect review by the pr: remove duplicated type

--- src/date-util/date-util.type.ts
+++ src/date-util/date-util.type.ts
@@ -4,4 +4,3 @@ import { Iso8601FormatType } from './date-util-base.type';
 export type CalcDatetimeOpts = Partial<DatetimeProperties>;
 export type DatetimeFormatOpts = Partial<FormatOpts<string>>;
 export type IsoDatetimeFormatOpts = Partial<FormatOpts<Iso8601FormatType>>;
-export type DateUnitType = 'seconds' | 'minutes' | 'hours';
