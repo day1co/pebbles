bdee7d83ce6dcd0909d7ccad1ca800d61c307ace
seunghwan <tmdghks0217@gmail.com>
2023-02-23 08:42:18 +0900

    add new consts in index.ts

--- src/date-util/index.ts
+++ src/date-util/index.ts
@@ -12,6 +12,11 @@ export {
   ONE_MINUTE_IN_SECOND,
   ONE_SECOND_IN_MILLI,
   TIMESTAMP_FORMAT,
+  DATE_FORMAT_DOT_TYPE,
+  DATE_FORMAT_US,
+  TIMEZONE_SEOUL,
+  TIMEZONE_PST,
+  TIMEZONE_TOKYO,
 } from './date-util.const';
 export type { DatetimeProperties, FormatOpts, LocalDateTimeFormatOpts } from './date-util.interface';
 export type { CalcDatetimeOpts, DatetimeFormatOpts, IsoDatetimeFormatOpts } from './date-util.type';
