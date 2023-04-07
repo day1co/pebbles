924b90c7e0d12f6cf01b4d31f7a5e673b4e0fe41
Hyun Jin Kim <costa.kim@day1company.co.kr>
2023-04-07 19:05:48 +0900

    feat: callback 대신 단일 키를 사용할 수 있도록 기능 추가

--- src/array-util/array-util.spec.ts
+++ src/array-util/array-util.spec.ts
@@ -2,7 +2,7 @@ import { ArrayUtil } from './array-util';
 
 describe('ArrayUtil', function () {
   describe('innerJoin', function () {
-    it('arr1 의 원소가 탈락하는 경우', function () {
+    describe('arr1 의 원소가 탈락하는 경우', function () {
       const arr1: { a: string; b: string }[] = [
         { a: '1', b: '1' },
         { a: '2', b: '2' },
@@ -12,19 +12,28 @@ describe('ArrayUtil', function () {
         { c: '1', d: '1' },
         { c: '2', d: '2' },
       ];
-      const callback1 = function (item: typeof arr1[0]) {
-        return item.a;
-      };
-      const callback2 = function (item: typeof arr2[0]) {
-        return item.c;
-      };
-      const result = ArrayUtil.innerJoin(arr1, arr2, callback1, callback2);
-      expect(result).toEqual([
+      const expected = [
         { a: '1', b: '1', c: '1', d: '1' },
         { a: '2', b: '2', c: '2', d: '2' },
-      ]);
+      ];
+      it('key1, key2 를 사용하는 경우', function () {
+        const result = ArrayUtil.innerJoin(arr1, arr2, 'a', 'c');
+        expect(result).toEqual(expected);
+      });
+
+      it('callback1, callback2 를 사용하는 경우', function () {
+        const callback1 = function (item: typeof arr1[0]) {
+          return item.a;
+        };
+        const callback2 = function (item: typeof arr2[0]) {
+          return item.c;
+        };
+        const result = ArrayUtil.innerJoin(arr1, arr2, callback1, callback2);
+        expect(result).toEqual(expected);
+      });
     });
-    it('arr2 의 원소가 탈락하는 경우', function () {
+
+    describe('arr2 의 원소가 탈락하는 경우', function () {
       const arr1: { a: string; b: string }[] = [
         { a: '2', b: '2' },
         { a: '3', b: '3' },
@@ -34,22 +43,29 @@ describe('ArrayUtil', function () {
         { c: '2', d: '2' },
         { c: '3', d: '3' },
       ];
-      const callback1 = function (item: typeof arr1[0]) {
-        return item.a;
-      };
-      const callback2 = function (item: typeof arr2[0]) {
-        return item.c;
-      };
-      const result = ArrayUtil.innerJoin(arr1, arr2, callback1, callback2);
-      expect(result).toEqual([
+      const expected = [
         { a: '2', b: '2', c: '2', d: '2' },
         { a: '3', b: '3', c: '3', d: '3' },
-      ]);
+      ];
+      it('key1, key2 를 사용하는 경우', function () {
+        const result = ArrayUtil.innerJoin(arr1, arr2, 'a', 'c');
+        expect(result).toEqual(expected);
+      });
+      it('callback1, callback2 를 사용하는 경우', function () {
+        const callback1 = function (item: typeof arr1[0]) {
+          return item.a;
+        };
+        const callback2 = function (item: typeof arr2[0]) {
+          return item.c;
+        };
+        const result = ArrayUtil.innerJoin(arr1, arr2, callback1, callback2);
+        expect(result).toEqual(expected);
+      });
     });
   });
 
   describe('leftJoin', () => {
-    it('arr1 의 원소가 더 많은 경우', function () {
+    describe('arr1 의 원소가 더 많은 경우', function () {
       const arr1: { a: string; b: string }[] = [
         { a: '1', b: '1' },
         { a: '2', b: '2' },
@@ -59,19 +75,26 @@ describe('ArrayUtil', function () {
         { c: '1', d: '1' },
         { c: '2', d: '2' },
       ];
-      const callback1 = function (item: typeof arr1[0]) {
-        return item.a;
-      };
-      const callback2 = function (item: typeof arr2[0]) {
-        return item.c;
-      };
-
-      const result = ArrayUtil.leftJoin(arr1, arr2, callback1, callback2);
-      expect(result).toEqual([
+      const expected = [
         { a: '1', b: '1', c: '1', d: '1' },
         { a: '2', b: '2', c: '2', d: '2' },
         { a: '3', b: '3', c: undefined, d: undefined },
-      ]);
+      ];
+      it('key1, key2 를 사용하는 경우', function () {
+        const result = ArrayUtil.leftJoin(arr1, arr2, 'a', 'c');
+        expect(result).toEqual(expected);
+      });
+      it('callback1, callback2 를 사용하는 경우', function () {
+        const callback1 = function (item: typeof arr1[0]) {
+          return item.a;
+        };
+        const callback2 = function (item: typeof arr2[0]) {
+          return item.c;
+        };
+
+        const result = ArrayUtil.leftJoin(arr1, arr2, callback1, callback2);
+        expect(result).toEqual(expected);
+      });
     });
     it('arr2 의 원소가 더 많은 경우', function () {
       const arr1: { a: string; b: string }[] = [
--- src/array-util/array-util.ts
+++ src/array-util/array-util.ts
@@ -1,10 +1,20 @@
 export namespace ArrayUtil {
+  export function innerJoin<T, U>(arr1: T[], arr2: U[], key1: keyof T, key2: keyof U): Array<T & U>;
   export function innerJoin<T, U, K>(
     arr1: T[],
     arr2: U[],
     callback1: (item: T) => K,
     callback2: (item: U) => K
+  ): Array<T & U>;
+  export function innerJoin<T, U, K>(
+    arr1: T[],
+    arr2: U[],
+    arg1: keyof T | ((item: T) => K),
+    arg2: keyof U | ((item: U) => K)
   ): Array<T & U> {
+    const callback1 = typeof arg1 === 'function' ? arg1 : (item: T): K => item[arg1] as K;
+    const callback2 = typeof arg2 === 'function' ? arg2 : (item: U): K => item[arg2] as K;
+
     const map = new Map<K, U>();
     for (const item of arr2) {
       const key = callback2(item);
@@ -23,12 +33,22 @@ export namespace ArrayUtil {
     return result;
   }
 
+  export function leftJoin<T, U>(arr1: T[], arr2: U[], key1: keyof T, key2: keyof U): Array<T & Partial<U>>;
   export function leftJoin<T, U, K>(
     arr1: T[],
     arr2: U[],
     callback1: (item: T) => K,
     callback2: (item: U) => K
+  ): Array<T & Partial<U>>;
+  export function leftJoin<T, U, K>(
+    arr1: T[],
+    arr2: U[],
+    arg1: keyof T | ((item: T) => K),
+    arg2: keyof U | ((item: U) => K)
   ): Array<T & Partial<U>> {
+    const callback1 = typeof arg1 === 'function' ? arg1 : (item: T): K => item[arg1] as K;
+    const callback2 = typeof arg2 === 'function' ? arg2 : (item: U): K => item[arg2] as K;
+
     const map = new Map<K, U>();
     for (const item of arr2) {
       const key = callback2(item);
