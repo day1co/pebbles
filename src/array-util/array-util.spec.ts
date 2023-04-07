c9c282448c675b8e19946b5e9ad5f08851e13b45
Hyun Jin Kim <costa.kim@day1company.co.kr>
2023-04-07 19:19:32 +0900

    fix: array-util 스펙 수정

--- src/array-util/array-util.spec.ts
+++ src/array-util/array-util.spec.ts
@@ -96,28 +96,35 @@ describe('ArrayUtil', function () {
         expect(result).toEqual(expected);
       });
     });
-    it('arr2 의 원소가 더 많은 경우', function () {
+    describe('arr2 의 원소가 더 많은 경우', function () {
       const arr1: { a: string; b: string }[] = [
-        { a: '1', b: '1' },
         { a: '2', b: '2' },
+        { a: '3', b: '3' },
       ];
       const arr2: { c: string; d: string }[] = [
         { c: '1', d: '1' },
         { c: '2', d: '2' },
         { c: '3', d: '3' },
       ];
-      const callback1 = function (item: typeof arr1[0]) {
-        return item.a;
-      };
-      const callback2 = function (item: typeof arr2[0]) {
-        return item.c;
-      };
-
-      const result = ArrayUtil.leftJoin(arr1, arr2, callback1, callback2);
-      expect(result).toEqual([
-        { a: '1', b: '1', c: '1', d: '1' },
+      const expected = [
         { a: '2', b: '2', c: '2', d: '2' },
-      ]);
+        { a: '3', b: '3', c: '3', d: '3' },
+      ];
+      it('key1, key2 를 사용하는 경우', function () {
+        const result = ArrayUtil.leftJoin(arr1, arr2, 'a', 'c');
+        expect(result).toEqual(expected);
+      });
+      it('callback1, callback2 를 사용하는 경우', function () {
+        const callback1 = function (item: typeof arr1[0]) {
+          return item.a;
+        };
+        const callback2 = function (item: typeof arr2[0]) {
+          return item.c;
+        };
+
+        const result = ArrayUtil.leftJoin(arr1, arr2, callback1, callback2);
+        expect(result).toEqual(expected);
+      });
     });
   });
 });
