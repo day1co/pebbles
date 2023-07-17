07805fd920f430e7eac6a78bc96892ccfed2fa0f
Hyun Jin Kim <costa.kim@day1company.co.kr>
2023-07-17 18:45:08 +0900

    feat: MapUtil.groupBy 추가

--- src/map-util/map-util.spec.ts
+++ src/map-util/map-util.spec.ts
@@ -46,11 +46,62 @@ describe('MapUtil', () => {
       expect(result).toEqual(expectedMap);
     });
 
-    test('should return an empty map when given an empty array', () => {
+    it('should return an empty map when given an empty array', () => {
       const array: any[] = [];
       const expectedMap = new Map();
       const result = MapUtil.groupByKey(array, 'name');
       expect(result).toEqual(expectedMap);
     });
+  });
+
+  describe('groupBy', () => {
+    it('should group an array of objects by a specified callback', () => {
+      const array = [
+        { id: 1, name: 'John' },
+        { id: 2, name: 'Jane' },
+        { id: 3, name: 'John' },
+        { id: 4, name: 'Mary' },
+        { id: 5, name: null },
+      ];
+
+      const expectedMap = new Map([
+        [
+          'John',
+          [
+            { id: 1, name: 'John' },
+            { id: 3, name: 'John' },
+          ],
+        ],
+        ['Jane', [{ id: 2, name: 'Jane' }]],
+        ['Mary', [{ id: 4, name: 'Mary' }]],
+        [null, [{ id: 5, name: null }]],
+      ]);
+      const result = MapUtil.groupBy(array, (item) => item.name);
+      expect(result).toEqual(expectedMap);
+    });
+  });
+
+  it('should return an empty map when given an empty array', () => {
+    const array: any[] = [];
+    const expectedMap = new Map();
+    const result = MapUtil.groupBy(array, () => 'name');
+    expect(result).toEqual(expectedMap);
+  });
+
+  it('should skip if callback returns undefined', () => {
+    const array = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Jane' },
+      { id: 3, name: 'John' },
+      { id: 4, name: 'Mary' },
+      { id: 5, name: null },
+    ];
+
+    const expectedMap = new Map([]);
+
+    const result = MapUtil.groupBy(array, (item) => {
+      return (item as any).invalidKey;
+    });
+    expect(result).toEqual(expectedMap);
   });
 });
