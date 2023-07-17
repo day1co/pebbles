902aa13be5550a6320a8582cc77069e65e913a8e
Hyun Jin Kim <costa.kim@day1company.co.kr>
2023-07-17 18:45:08 +0900

    feat: MapUtil.groupBy 추가

--- src/map-util/map-util.ts
+++ src/map-util/map-util.ts
@@ -12,8 +12,15 @@ export namespace MapUtil {
   }
 
   export function groupByKey<T, K extends keyof T>(array: T[], key: K): Map<T[K], T[]> {
+    return groupBy(array, (item) => item[key]);
+  }
+
+  export function groupBy<T, K>(array: T[], callback: (item: T) => K): Map<K, T[]> {
     return array.reduce((map, item) => {
-      const mapKey = item[key];
+      const mapKey = callback(item);
+      if (mapKey === undefined) {
+        return map;
+      }
       const collection = map.get(mapKey) ?? map.set(mapKey, []).get(mapKey);
       collection.push(item);
       return map;
