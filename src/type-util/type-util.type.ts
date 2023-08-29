c60084eaf7732bfc27327d50177c14626e99db40
taehuikim <taehuikim@day1company.co.kr>
2023-08-29 10:47:25 +0900

    feat: restone-policy에 정의된 타입 이전(Nullable, Arrayable)

--- src/type-util/type-util.type.ts
+++ src/type-util/type-util.type.ts
@@ -7,3 +7,5 @@ export type PickWithPartial<T, K extends keyof T> = Pick<T, K> & Partial<T>;
 export type RequiredPartialProps<T, C extends keyof T> = T & {
   [K in C]-?: T[K];
 };
+export type Nullable<T> = T | null | undefined;
+export type Arrayable<T> = T | Array<T>;
