24802438c855d4a7ddf2d64995e75de2b164c7c2
YoungTaek <soomtong@gmail.com>
2023-12-20 14:37:33 +0900

    money-util: fix typing for unused vars

--- src/money-util/money.type.ts
+++ src/money-util/money.type.ts
@@ -1,7 +1,7 @@
 export type Locale = 'ko-KR' | 'en-US' | 'ja-JP';
 export type Currency = 'USD' | 'KRW' | 'JPY';
-export type LocaleMap<T> = {
-  [Property in Currency]: Locale;
+export type LocaleMap<T extends Currency> = {
+  [Property in T]: Locale;
 };
 export interface ConvertLocalCurrencyFormatOpts {
   unit?: string;
