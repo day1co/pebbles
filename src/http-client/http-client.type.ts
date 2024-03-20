b8e7607cfc8edb846e7a5d5fc894d9f0e9c9c15a
anniemon <seojung19@gmail.com>
2024-03-20 10:26:12 +0900

    http-client: add AxiosErrorType, AxiosResponseType

--- src/http-client/http-client.type.ts
+++ src/http-client/http-client.type.ts
@@ -1,7 +1,10 @@
-import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import type { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 
 export type HttpReqConfig = AxiosRequestConfig;
 export type HttpResponse<T> = AxiosResponse<T>;
 
 /** @deprecated */
 export type HttpRes<T> = HttpResponse<T>;
+
+export type AxiosErrorType = AxiosError;
+export type AxiosResponseType = AxiosResponse<{ data: { name: string; message: string } }>;
