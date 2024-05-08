8830f7cbf4bd97a0216721314e09fb6de30a40cc
혜영 <hysung714@naver.com>
2024-05-08 14:59:41 +0900

    fix : index 에 conflict-exception 추가

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -6,6 +6,7 @@ export {
   NotFoundException,
   TokenExpiredException,
   UnauthorizedException,
+  ConflictException,
 } from './client-exception';
 export { CustomException } from './custom-exception';
 export { DataException, InternalServerException, ServerException, UnknownException } from './server-exception';
