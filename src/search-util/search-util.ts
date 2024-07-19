1a258e0427abef35a2df78f5e83c912c28fdeb70
taehuikim <taehuikim9307@gmail.com>
2024-07-19 15:32:23 +0900

    feat: binary search 추가

---
+++ src/search-util/search-util.ts
@@ -0,0 +1,23 @@
+export namespace SearchUtil {
+  export function binarySearch<T>(arr: T[], target: T, compareFn: (target: T, item: T) => number): T | undefined {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+      const mid = left + Math.floor((right - left) / 2);
+      const comparison = compareFn(target, arr[mid]);
+
+      if (comparison === 0) {
+        return arr[mid];
+      }
+
+      if (comparison < 0) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return undefined;
+  }
+}
