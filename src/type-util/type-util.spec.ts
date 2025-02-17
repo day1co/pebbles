3f063faa739c3e8c0734d2fa8af28bc37677f7ac
jaehong <jaehong2782@gmail.com>
2025-02-17 15:33:16 +0900

    refactor: type-util remove namespace

--- src/type-util/type-util.spec.ts
+++ src/type-util/type-util.spec.ts
@@ -1,45 +1,42 @@
-import { TypeUtil } from './type-util';
+import { makeLiteralTypeList } from './type-util';
 import type { PickWithPartial, RequiredPartialProps } from './type-util.type';
 
-describe('TypeUtil', () => {
-  describe('makeLiteralTypeList', () => {
-    const { makeLiteralTypeList } = TypeUtil;
-    it('should return array', () => {
-      expect(makeLiteralTypeList('foo', 'bar', 'baz', 'qux', 'quux')).toEqual(['foo', 'bar', 'baz', 'qux', 'quux']);
-      expect(makeLiteralTypeList(1, 3, 2, 4, 5)).toEqual([1, 3, 2, 4, 5]);
-      expect(makeLiteralTypeList('foo', 1, 'bar', 2, undefined)).toEqual(['foo', 1, 'bar', 2, undefined]);
-    });
+describe('makeLiteralTypeList', () => {
+  it('should return array', () => {
+    expect(makeLiteralTypeList('foo', 'bar', 'baz', 'qux', 'quux')).toEqual(['foo', 'bar', 'baz', 'qux', 'quux']);
+    expect(makeLiteralTypeList(1, 3, 2, 4, 5)).toEqual([1, 3, 2, 4, 5]);
+    expect(makeLiteralTypeList('foo', 1, 'bar', 2, undefined)).toEqual(['foo', 1, 'bar', 2, undefined]);
   });
+});
 
-  describe('PickWithPartial', () => {
-    it('should return ok', () => {
-      interface MockInterface {
-        prop1: number;
-        prop2: string;
-        prop3: boolean;
-      }
-      const implementedObj1: PickWithPartial<MockInterface, 'prop1'> = { prop1: 1 };
-      const implementedObj2: PickWithPartial<MockInterface, 'prop1' | 'prop2'> = { prop1: 2, prop2: 'string' };
-      expect(implementedObj1).toHaveProperty('prop1');
-      expect(implementedObj2).not.toHaveProperty('prop3');
-    });
+describe('PickWithPartial', () => {
+  it('should return ok', () => {
+    interface MockInterface {
+      prop1: number;
+      prop2: string;
+      prop3: boolean;
+    }
+    const implementedObj1: PickWithPartial<MockInterface, 'prop1'> = { prop1: 1 };
+    const implementedObj2: PickWithPartial<MockInterface, 'prop1' | 'prop2'> = { prop1: 2, prop2: 'string' };
+    expect(implementedObj1).toHaveProperty('prop1');
+    expect(implementedObj2).not.toHaveProperty('prop3');
   });
+});
 
-  describe('RequiredPartialProps', () => {
-    it('should return ok', () => {
-      interface MockInterface {
-        prop1: number;
-        prop2?: string;
-        prop3?: boolean;
-      }
-      const implementedObj1: RequiredPartialProps<MockInterface, 'prop2'> = { prop1: 1, prop2: 'hello' };
-      const implementedObj2: RequiredPartialProps<MockInterface, 'prop2' | 'prop3'> = {
-        prop1: 2,
-        prop2: 'world',
-        prop3: true,
-      };
-      expect(implementedObj1).toHaveProperty('prop2');
-      expect(implementedObj2).toHaveProperty('prop3');
-    });
+describe('RequiredPartialProps', () => {
+  it('should return ok', () => {
+    interface MockInterface {
+      prop1: number;
+      prop2?: string;
+      prop3?: boolean;
+    }
+    const implementedObj1: RequiredPartialProps<MockInterface, 'prop2'> = { prop1: 1, prop2: 'hello' };
+    const implementedObj2: RequiredPartialProps<MockInterface, 'prop2' | 'prop3'> = {
+      prop1: 2,
+      prop2: 'world',
+      prop3: true,
+    };
+    expect(implementedObj1).toHaveProperty('prop2');
+    expect(implementedObj2).toHaveProperty('prop3');
   });
 });
