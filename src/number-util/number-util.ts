054a884131b768edd81385daf11e47f2b863cefb
jongyi kim (hibiya) <jikim@day1company.co.kr>
2025-02-17 17:37:40 +0900

    refactor: number-util: valueOf(Number) removed (unused method)

--- src/number-util/number-util.ts
+++ src/number-util/number-util.ts
@@ -7,15 +7,6 @@ export function intValueOf(numStr: string): number {
   return result;
 }
 
-export function valueOfNumber(numStr: string): number {
-  const result = Number(numStr);
-
-  if (!numStr || !Number.isFinite(result)) {
-    throw new Error(`unable to convert "${numStr}" to number type`);
-  }
-  return result;
-}
-
 export function isNumeric(numStr: string): boolean {
   const num = Number(numStr);
   return !isNaN(num) && isFinite(num) && num === parseFloat(numStr);
