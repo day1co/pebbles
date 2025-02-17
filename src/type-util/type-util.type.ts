3eb2d38d4e78c066730cf20a950d6fe7e3fd9fd2
jaehong <jaehong2782@gmail.com>
2025-02-17 15:34:11 +0900

    feature: added Nullish type for clarity

--- src/type-util/type-util.type.ts
+++ src/type-util/type-util.type.ts
@@ -7,5 +7,8 @@ export type PickWithPartial<T, K extends keyof T> = Pick<T, K> & Partial<T>;
 export type RequiredPartialProps<T, C extends keyof T> = T & {
   [K in C]-?: T[K];
 };
-export type Nullable<T> = T | null | undefined;
+
+// https://github.com/day1coloso/coloso/pull/7036#issuecomment-2661939732
+export type Nullish<T> = T | null | undefined;
+export type Nullable<T> = T | null;
 export type Arrayable<T> = T | Array<T>;
