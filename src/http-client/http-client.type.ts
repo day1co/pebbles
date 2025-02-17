b744800c124e12138950a1fdfdb1ca30b9473880
jaehong <jaehong2782@gmail.com>
2025-02-17 15:40:30 +0900

    refactor: cleaning up deprecated types

--- src/http-client/http-client.type.ts
+++ src/http-client/http-client.type.ts
@@ -1,9 +1,5 @@
-import type { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export type HttpReqConfig = AxiosRequestConfig;
 export type HttpResponse<T, D = any> = AxiosResponse<T, D>;
-
-/** @deprecated */
-export type HttpRes<T, D = any> = HttpResponse<T, D>;
-
 export type HttpError<T = unknown, D = any> = AxiosError<T, D>;
