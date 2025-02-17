bc004a179f663b2e3913d4c112fa68b60026c47e
jaehong <jaehong2782@gmail.com>
2025-02-17 15:30:09 +0900

    refactor: number-util remove namespace

--- src/number-util/number-util.spec.ts
+++ src/number-util/number-util.spec.ts
@@ -1,113 +1,119 @@
-import { NumberUtil } from './number-util';
+import {
+  decimalRoundDown,
+  decimalRoundUp,
+  fromPermyriad,
+  intValueOf,
+  isNumeric,
+  parseNumber,
+  toPermyriad,
+} from './number-util';
 
-describe('NumberUtil', () => {
-  describe('intValueOf', () => {
-    it('should throw with incompatible input', () => {
-      expect(() => NumberUtil.intValueOf('abc')).toThrow();
-      expect(() => NumberUtil.intValueOf('4.5a')).toThrow();
-      expect(() => NumberUtil.intValueOf('1.2')).toThrow();
-      expect(() => NumberUtil.intValueOf('1.2.3')).toThrow();
-      expect(() => NumberUtil.intValueOf('')).toThrow();
-      expect(() => NumberUtil.intValueOf(String(Number.MIN_VALUE))).toThrow();
-      expect(() => NumberUtil.intValueOf(String(Number.EPSILON))).toThrow();
-    });
-    it('should return parameter in integer type', () => {
-      expect(NumberUtil.intValueOf('1')).toEqual(1);
-      expect(NumberUtil.intValueOf('-1')).toEqual(-1);
-      expect(NumberUtil.intValueOf('0')).toEqual(0);
-      expect(NumberUtil.intValueOf(String(Number.MAX_SAFE_INTEGER))).toEqual(Number.MAX_SAFE_INTEGER);
-      expect(NumberUtil.intValueOf(String(Number.MIN_SAFE_INTEGER))).toEqual(Number.MIN_SAFE_INTEGER);
-      expect(NumberUtil.intValueOf(String(Number.MAX_VALUE))).toEqual(Number.MAX_VALUE);
-    });
+describe('intValueOf', () => {
+  it('should throw with incompatible input', () => {
+    expect(() => intValueOf('abc')).toThrow();
+    expect(() => intValueOf('4.5a')).toThrow();
+    expect(() => intValueOf('1.2')).toThrow();
+    expect(() => intValueOf('1.2.3')).toThrow();
+    expect(() => intValueOf('')).toThrow();
+    expect(() => intValueOf(String(Number.MIN_VALUE))).toThrow();
+    expect(() => intValueOf(String(Number.EPSILON))).toThrow();
+  });
+  it('should return parameter in integer type', () => {
+    expect(intValueOf('1')).toEqual(1);
+    expect(intValueOf('-1')).toEqual(-1);
+    expect(intValueOf('0')).toEqual(0);
+    expect(intValueOf(String(Number.MAX_SAFE_INTEGER))).toEqual(Number.MAX_SAFE_INTEGER);
+    expect(intValueOf(String(Number.MIN_SAFE_INTEGER))).toEqual(Number.MIN_SAFE_INTEGER);
+    expect(intValueOf(String(Number.MAX_VALUE))).toEqual(Number.MAX_VALUE);
+  });
+});
+
+describe('parseNumber', () => {
+  it('should throw with incompatible input', () => {
+    expect(() => parseNumber('abc')).toThrow();
+    expect(() => parseNumber('4a')).toThrow();
+    expect(() => parseNumber('1.2.3')).toThrow();
+    expect(() => parseNumber('')).toThrow();
+  });
+  it('should return parameter in number type', () => {
+    expect(parseNumber('-1')).toEqual(-1);
+    expect(parseNumber('0')).toEqual(0);
+    expect(parseNumber('1.2')).toEqual(1.2);
+    expect(parseNumber('-1.2')).toEqual(-1.2);
+    expect(parseNumber('-93339.228883747849')).toEqual(-93339.22888374786);
+    expect(parseNumber('9488848.29000004833')).toEqual(9488848.290000048);
+  });
+});
+
+describe('isNumeric', () => {
+  it('should equal to all', () => {
+    expect(isNumeric('0')).toEqual(true);
+    expect(isNumeric('1')).toEqual(true);
+    expect(isNumeric('01')).toEqual(true);
+    expect(isNumeric('0.1')).toEqual(true);
+    expect(isNumeric('-10')).toEqual(true);
+    expect(isNumeric('-10.1231234')).toEqual(true);
+  });
+  it('should not equal to all', () => {
+    expect(isNumeric('a')).toEqual(false);
+    expect(isNumeric('0a')).toEqual(false);
+    expect(isNumeric('0xa')).toEqual(false);
+    expect(isNumeric('sdfsdfsdfa')).toEqual(false);
+    expect(isNumeric('\\http')).toEqual(false);
+    expect(isNumeric('1.2.3')).toEqual(false);
+    expect(isNumeric('')).toEqual(false);
+    expect(isNumeric('  ')).toEqual(false);
+  });
+});
+
+describe('toPermyriad', () => {
+  it('should throw', () => {
+    expect(() => toPermyriad(NaN)).toThrow();
+  });
+  it('should convert to permyriad', () => {
+    expect(toPermyriad(100)).toEqual(1_000_000);
+    expect(toPermyriad(0.01)).toEqual(100);
+    expect(toPermyriad(0.00001)).toEqual(0.1);
+    expect(toPermyriad(10.005)).toEqual(100050);
+    expect(toPermyriad(0)).toEqual(0);
+  });
+});
+
+describe('fromPermyriad', () => {
+  it('should throw', () => {
+    expect(() => fromPermyriad(NaN)).toThrow();
+  });
+  it('should convert from permyriad', () => {
+    expect(fromPermyriad(100)).toEqual(0.01);
+    expect(fromPermyriad(0.1)).toEqual(0);
+    expect(fromPermyriad(0)).toEqual(0);
+  });
+});
+
+describe('decimalRoundUp & Down', () => {
+  it('should throw', () => {
+    expect(() => decimalRoundUp(NaN)).toThrow();
+    expect(() => decimalRoundDown(NaN)).toThrow();
   });
 
-  describe('valueOf', () => {
-    it('should throw with incompatible input', () => {
-      expect(() => NumberUtil.valueOf('abc')).toThrow();
-      expect(() => NumberUtil.valueOf('4a')).toThrow();
-      expect(() => NumberUtil.valueOf('1.2.3')).toThrow();
-      expect(() => NumberUtil.valueOf('')).toThrow();
-    });
-    it('should return parameter in number type', () => {
-      expect(NumberUtil.valueOf('-1')).toEqual(-1);
-      expect(NumberUtil.valueOf('0')).toEqual(0);
-      expect(NumberUtil.valueOf('1.2')).toEqual(1.2);
-      expect(NumberUtil.valueOf('-1.2')).toEqual(-1.2);
-      expect(NumberUtil.valueOf('-93339.228883747849')).toEqual(-93339.22888374786);
-      expect(NumberUtil.valueOf('9488848.29000004833')).toEqual(9488848.290000048);
-    });
+  it('should convert from decimal places up', () => {
+    expect(decimalRoundUp(100, 0)).toEqual(100);
+    expect(decimalRoundUp(100, 3)).toEqual(100000);
+    expect(decimalRoundUp(1.23456, 5)).toEqual(123456);
+    expect(decimalRoundUp(100.2, 5)).toEqual(10020000);
+    expect(decimalRoundUp(100.123456789, 2)).toEqual(10012);
+    expect(decimalRoundUp(100.123456789, 5)).toEqual(10012346);
+    expect(decimalRoundUp(203.502)).toEqual(20350);
+    expect(decimalRoundUp(203.509)).toEqual(20351);
   });
 
-  describe('isNumeric', () => {
-    it('should equal to all', () => {
-      expect(NumberUtil.isNumeric('0')).toEqual(true);
-      expect(NumberUtil.isNumeric('1')).toEqual(true);
-      expect(NumberUtil.isNumeric('01')).toEqual(true);
-      expect(NumberUtil.isNumeric('0.1')).toEqual(true);
-      expect(NumberUtil.isNumeric('-10')).toEqual(true);
-      expect(NumberUtil.isNumeric('-10.1231234')).toEqual(true);
-    });
-    it('should not equal to all', () => {
-      expect(NumberUtil.isNumeric('a')).toEqual(false);
-      expect(NumberUtil.isNumeric('0a')).toEqual(false);
-      expect(NumberUtil.isNumeric('0xa')).toEqual(false);
-      expect(NumberUtil.isNumeric('sdfsdfsdfa')).toEqual(false);
-      expect(NumberUtil.isNumeric('\\http')).toEqual(false);
-      expect(NumberUtil.isNumeric('1.2.3')).toEqual(false);
-      expect(NumberUtil.isNumeric('')).toEqual(false);
-      expect(NumberUtil.isNumeric('  ')).toEqual(false);
-    });
-  });
-
-  describe('toPermyriad', () => {
-    it('should throw', () => {
-      expect(() => NumberUtil.toPermyriad(NaN)).toThrow();
-    });
-    it('should convert to permyriad', () => {
-      expect(NumberUtil.toPermyriad(100)).toEqual(1_000_000);
-      expect(NumberUtil.toPermyriad(0.01)).toEqual(100);
-      expect(NumberUtil.toPermyriad(0.00001)).toEqual(0.1);
-      expect(NumberUtil.toPermyriad(10.005)).toEqual(100050);
-      expect(NumberUtil.toPermyriad(0)).toEqual(0);
-    });
-  });
-
-  describe('fromPermyriad', () => {
-    it('should throw', () => {
-      expect(() => NumberUtil.fromPermyriad(NaN)).toThrow();
-    });
-    it('should convert from permyriad', () => {
-      expect(NumberUtil.fromPermyriad(100)).toEqual(0.01);
-      expect(NumberUtil.fromPermyriad(0.1)).toEqual(0);
-      expect(NumberUtil.fromPermyriad(0)).toEqual(0);
-    });
-  });
-
-  describe('decimalRoundUp & Down', () => {
-    it('should throw', () => {
-      expect(() => NumberUtil.decimalRoundUp(NaN)).toThrow();
-      expect(() => NumberUtil.decimalRoundDown(NaN)).toThrow();
-    });
-
-    it('should convert from decimal places up', () => {
-      expect(NumberUtil.decimalRoundUp(100, 0)).toEqual(100);
-      expect(NumberUtil.decimalRoundUp(100, 3)).toEqual(100000);
-      expect(NumberUtil.decimalRoundUp(1.23456, 5)).toEqual(123456);
-      expect(NumberUtil.decimalRoundUp(100.2, 5)).toEqual(10020000);
-      expect(NumberUtil.decimalRoundUp(100.123456789, 2)).toEqual(10012);
-      expect(NumberUtil.decimalRoundUp(100.123456789, 5)).toEqual(10012346);
-      expect(NumberUtil.decimalRoundUp(203.502)).toEqual(20350);
-      expect(NumberUtil.decimalRoundUp(203.509)).toEqual(20351);
-    });
-
-    it('should convert from decimal places down', () => {
-      expect(NumberUtil.decimalRoundDown(100000, 0)).toEqual(100000);
-      expect(NumberUtil.decimalRoundDown(100000, 3)).toEqual(100);
-      expect(NumberUtil.decimalRoundDown(123456, 5)).toEqual(1.23456);
-      expect(NumberUtil.decimalRoundDown(10020000, 5)).toEqual(100.2);
-      expect(NumberUtil.decimalRoundDown(10012, 2)).toEqual(100.12);
-      expect(NumberUtil.decimalRoundDown(10012345, 5)).toEqual(100.12345);
-      expect(NumberUtil.decimalRoundDown(20351)).toEqual(203.51);
-    });
+  it('should convert from decimal places down', () => {
+    expect(decimalRoundDown(100000, 0)).toEqual(100000);
+    expect(decimalRoundDown(100000, 3)).toEqual(100);
+    expect(decimalRoundDown(123456, 5)).toEqual(1.23456);
+    expect(decimalRoundDown(10020000, 5)).toEqual(100.2);
+    expect(decimalRoundDown(10012, 2)).toEqual(100.12);
+    expect(decimalRoundDown(10012345, 5)).toEqual(100.12345);
+    expect(decimalRoundDown(20351)).toEqual(203.51);
   });
 });
--- src/number-util/number-util.ts
+++ src/number-util/number-util.ts
@@ -1,61 +1,59 @@
-export namespace NumberUtil {
-  export function intValueOf(numStr: string): number {
-    const result = Number(numStr);
+export function intValueOf(numStr: string): number {
+  const result = Number(numStr);
 
-    if (!numStr || !Number.isInteger(result)) {
-      throw new Error(`unable to convert "${numStr}" to integer type`);
-    }
-    return result;
+  if (!numStr || !Number.isInteger(result)) {
+    throw new Error(`unable to convert "${numStr}" to integer type`);
   }
-
-  export function valueOf(numStr: string): number {
-    const result = Number(numStr);
-
-    if (!numStr || !Number.isFinite(result)) {
-      throw new Error(`unable to convert "${numStr}" to number type`);
-    }
-    return result;
-  }
-
-  export function isNumeric(numStr: string): boolean {
-    const num = Number(numStr);
-    return !isNaN(num) && isFinite(num) && num === parseFloat(numStr);
-  }
-
-  export const toPermyriad = (value: number, digits = 2): number => {
-    if (isNaN(value)) {
-      throw new Error(`unable to convert "${value}" to number type`);
-    }
-
-    return Number((value * 10_000).toFixed(digits));
-  };
-
-  export const fromPermyriad = (value: number, digits = 2): number => {
-    if (isNaN(value)) {
-      throw new Error(`unable to convert "${value}" to number type`);
-    }
-
-    return Number((value / 10_000).toFixed(digits));
-  };
-
-  export const decimalRoundUp = (floatValue: number, decimal = 2) => {
-    if (isNaN(floatValue)) {
-      throw new Error(`unable to convert "${floatValue}" to number type`);
-    }
-    if (decimal === 0) {
-      return Math.round(floatValue);
-    }
-    const factor = Math.pow(10, decimal);
-    return Math.round(floatValue * factor);
-  };
-
-  export const decimalRoundDown = (value: number, decimal = 2) => {
-    if (isNaN(value)) {
-      throw new Error(`unable to convert "${value}" to number type`);
-    }
-    if (decimal === 0) {
-      return value;
-    }
-    return value / Math.pow(10, decimal);
-  };
+  return result;
 }
+
+export function parseNumber(numStr: string): number {
+  const result = Number(numStr);
+
+  if (!numStr || !Number.isFinite(result)) {
+    throw new Error(`unable to convert "${numStr}" to number type`);
+  }
+  return result;
+}
+
+export function isNumeric(numStr: string): boolean {
+  const num = Number(numStr);
+  return !isNaN(num) && isFinite(num) && num === parseFloat(numStr);
+}
+
+export const toPermyriad = (value: number, digits = 2): number => {
+  if (isNaN(value)) {
+    throw new Error(`unable to convert "${value}" to number type`);
+  }
+
+  return Number((value * 10_000).toFixed(digits));
+};
+
+export const fromPermyriad = (value: number, digits = 2): number => {
+  if (isNaN(value)) {
+    throw new Error(`unable to convert "${value}" to number type`);
+  }
+
+  return Number((value / 10_000).toFixed(digits));
+};
+
+export const decimalRoundUp = (floatValue: number, decimal = 2) => {
+  if (isNaN(floatValue)) {
+    throw new Error(`unable to convert "${floatValue}" to number type`);
+  }
+  if (decimal === 0) {
+    return Math.round(floatValue);
+  }
+  const factor = Math.pow(10, decimal);
+  return Math.round(floatValue * factor);
+};
+
+export const decimalRoundDown = (value: number, decimal = 2) => {
+  if (isNaN(value)) {
+    throw new Error(`unable to convert "${value}" to number type`);
+  }
+  if (decimal === 0) {
+    return value;
+  }
+  return value / Math.pow(10, decimal);
+};
