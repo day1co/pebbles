5e8b746ea37c5656cf82f34cf5769c7c8faabf6f
jongyi kim (hibiya) <jikim@day1company.co.kr>
2025-02-17 17:35:49 +0900

    refactor: map-util: removed getMapValue (unused method)

--- src/map-util/map-util.ts
+++ src/map-util/map-util.ts
@@ -1,18 +1,3 @@
-/**
- * map 에서 key 에 해당하는 값을 반환한다. 값이 undefined 이면 defaultValue 를 반환한다.
- *
- * @param map
- * @param key
- * @param defaultValue
- */
-
-export function getMapValue<K, V>(map: Map<K, V>, key: K): V | undefined;
-export function getMapValue<K, V>(map: Map<K, V>, key: K, defaultValue: V): V;
-export function getMapValue<K, V>(map: Map<K, V>, key: K, defaultValue?: V): V | undefined {
-  const value = map.get(key);
-  return value === undefined ? defaultValue : value;
-}
-
 export function groupByKey<T, K extends keyof T>(array: T[], key: K): Map<T[K], T[]> {
   return groupBy(array, (item) => item[key]);
 }
